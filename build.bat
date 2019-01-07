60ce2303d863013a9115c3c26dd3fdd622351ae4
jonnerloller <chen.enhan.jonathan@gmail.com>
2019-01-08 00:34:37 +0800

    Update for clang-cl
    Moved clang-cl environment setup to windows_setup.bat . build.bat is now just a glorified makefile...

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
-set CFLAGS=-m64
-set CXXFLAGS=-m64
 mkdir build 2> NUL
 cd build
 call conan install .. -s compiler=clang-cl -s arch=x86_64
--- windows_setup.bat
+++ windows_setup.bat
@@ -1,1 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set CC=clang-cl
+set CXX=clang-cl
+set CFLAGS=-m64
+set CXXFLAGS=-m64
