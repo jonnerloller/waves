29cdf75aeb745293324c5176c48fbb072fc44e5c
jonnerloller <chen.enhan.jonathan@gmail.com>
2019-01-09 23:31:38 +0800

    Update test_ci.cpp

--- tests/test_ci.cpp
+++ tests/test_ci.cpp
@@ -4,6 +4,20 @@
 
 #include <glm/glm.hpp>
 
+#if defined(RELAXED_CONSTEXPR)
+#define TEST(X) X
+#define CONSTEXPR
+#else
+#define TEST(X) static_test<X>()
+#define CONSTEXPR constexpr
+#endif
+
+template<bool B> bool static_test()
+{
+  static_assert(B);
+  return B;
+}
+
 TEST_CASE("Most Basic Test Case")
 {
   constexpr int a = 0;
